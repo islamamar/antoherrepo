eadfb98b5eb5b7b2d141ba39801155b701376eda
islamamar <161100@ppu.edu.ps>
2022-04-13 13:09:03 +0300

    adding sum function

--- JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -9,13 +9,20 @@ package javaapplication10;
  *
  * @author user
  */
-public class JavaApplication10 {
+public class JavaApplication10 { 
+    
+    
+    
+    public static int sum (int x , int y ){
+        return x+y ; 
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here 
+        System.out.println(sum(3,4));
     }
     
 }
